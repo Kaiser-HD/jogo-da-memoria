4050b1a06f2de6079e342a9e4fcaf6712837d140
Kaiser-HD <pedrosvaldo14@gmail.com>
2023-07-04 18:31:35 -0300

    feat: chow cards on windows

--- src.js
+++ src.js
@@ -10,6 +10,50 @@ const imagens = [
     './assets/tanjiro.jpg',
 ]
 
+imagens.sort(comparador);
+
+function comparador() { 
+    return Math.random() - 0.5; 
+}
+
+const dubble = []
+
 const verso = './assets/icon.png'
 
-console.log(imagens)
+function creatCard(qtd) {
+    const mesa = document.querySelector('.mesa-de-jogos')
+
+    for (let i = 0; i < qtd; i++) {
+        mesa.innerHTML += `
+        <div class="cartao" onclick=flip(this)>
+            <div class="frente">
+                <img src=${dubble[i]} alt="personagem douma">
+            </div>
+            <div class="verso">
+                <img src="./assets/icon.png" alt="icon Demon Slayer">
+            </div>
+        </div>`
+    }
+}
+
+function flip(element) {
+    element.classList.toggle('flip');
+}
+
+function selectCards(qtd) {
+    const x = qtd / 2
+    for (let i = 0; i < x; i++) {
+        dubble.push(imagens[i])
+        dubble.push(imagens[i])
+    }
+
+    dubble.sort(comparador);
+
+    function comparador() { 
+        return Math.random() - 0.5; 
+    }
+
+    creatCard(qtd)
+}
+
+selectCards(10)
