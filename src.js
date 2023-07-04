7d6e47536bbb394f59f19c50ae722e1e15674ad7
Kaiser-HD <pedrosvaldo14@gmail.com>
2023-07-04 12:04:16 -0300

    feat: create array

---
+++ src.js
@@ -0,0 +1,15 @@
+const imagens = [
+    './assets/muzan.jpg',
+    './assets/nggyu.gif',
+    './assets/sup1.jpg',
+    './assets/sup2.jpg',
+    './assets/sup3.jpg',
+    './assets/sup4.jpg',
+    './assets/sup5.png',
+    './assets/sup6.png',
+    './assets/tanjiro.jpg',
+]
+
+const verso = './assets/icon.png'
+
+console.log(imagens)
